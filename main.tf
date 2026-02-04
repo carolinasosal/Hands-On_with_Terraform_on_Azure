23b8784df893688c9bd2717d9d137b8a14403335
carol <carolinasosal@gmail.com>
2026-02-04 12:19:25 +0000

    terraform files

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ provider "azurerm" {
   features {}
   #skip_provider_registration = true
   subscription_id = "80ea84e8-afce-4851-928a-9e2219724c69"
+  resource_provider_registrations = "none"
 }
 
 resource "azurerm_resource_group" "rg" {
@@ -33,6 +34,6 @@ module "securestorage" {
   version              = "1.0.0"
   location             = azurerm_resource_group.rg.location
   resource_group_name  = azurerm_resource_group.rg.name
-  storage_account_name = "securestorage123"
+  storage_account_name = "securestorage"
 }
 
